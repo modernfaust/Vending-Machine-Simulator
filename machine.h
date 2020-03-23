23ba80d159da91a07b5ef0f269726ce38d7c3700
modernfaust <60478814+modernfaust@users.noreply.github.com>
2020-03-23 11:57:22 -0400

    Create machine.h

---
+++ machine.h
@@ -0,0 +1,13 @@
+#include <machine.c>
+#define NICKEL 5
+#define DIME 10
+#define QUARTER 25
+#define n NICKEL
+#define N NICKEL
+#define d DIME
+#define D DIME
+#define q QUARTER
+#define Q QUARTER
+void machine(int,int);
+
+//THIS IS THE HEADER FILE. CONTAINS ALL THE CONSTANT DEFINITIONS AND FUNCTION CALLS, TO BE FED TO MAIN
