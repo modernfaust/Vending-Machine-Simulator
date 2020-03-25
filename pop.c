712b6401bb6228b96bef029ff7f48e8763a7c6d4
modernfaust <60478814+modernfaust@users.noreply.github.com>
2020-03-24 21:56:21 -0400

    Update pop.c
    Functional, except that the machine needs an additional loop to keep it running after dispensing a pop.

--- pop.c
+++ pop.c
@@ -4,32 +4,35 @@
 #define NICKEL 5
 #define DIME 10
 #define QUARTER 25
-#define n NICKEL
-#define N NICKEL
-#define d DIME
-#define D DIME
-#define q QUARTER
-#define Q QUARTER
-void
-machine(int coins,int price){
-switch(coins){
-	case NICKEL:
-		coins=coins+NICKEL;
 
-	case DIME:
-		coins=coins+DIME;
+int
+machine(char choice,int *coins,int price){
+if (choice == 'r' | choice == 'R'){
+        return 1;
+}
+else if (choice == 'd' | choice == 'D'){
+        *coins=*coins+DIME;
+}
+else if (choice == 'n' | choice == 'N'){
+        *coins=*coins+NICKEL;
+}
+else if (choice == 'q' | choice == 'Q'){
+        *coins=*coins+QUARTER;
+}
+else{
+        printf("Unknown coin rejected.\n");
+}
 
-	case QUARTER:
-		coins=coins+QUARTER;
-}
-printf("You have inserted a total of %d cents",coins);
+printf("You have inserted a total of %d cents\n",*coins);
 
-if (coins<price){
-    printf("Please insert %d more cents",price-coins);
+if (*coins<price){
+    printf("Please insert %d more cents\n",price-*coins);
 }
-if (coins > price){
-    printf("Pop is dispensed. Thank you for your business! Please come again.");
+if (*coins >= price){
+    printf("Pop is dispensed. Thank you for your business! Please come again.\n");
+        return 1;
 }
+return 0;
 }
 
 void
@@ -39,7 +42,7 @@ int dimes=floor(change/DIME);
 int temp=dimes*DIME;
 int nickels=floor((change-temp)/NICKEL);
 if (change <= 0){
-	exit(0);
+        exit(0);
 }
 printf("Change given: %d cents as %d dime(s) and %d nickel(s).\n",change,dimes,nickels);
 }
@@ -47,29 +50,38 @@ printf("Change given: %d cents as %d dime(s) and %d nickel(s).\n",change,dimes,n
 void
 main(int argc, char *argv[]){
 if (argc<=1){
-	printf("Please specify selling price as command line argument.\n");
-	printf("Usage: pop [price]\n");
-	exit(0);
+        printf("Please specify selling price as command line argument.\n");
+        printf("Usage: pop [price]\n");
+        exit(0);
 }
 int price=atoi(argv[1]);
 int coins=0;
+char choice;
+int done=0;
 
 if (price <30 | price > 105){
-	printf("Price must be from 30 to 105 cents inclusive\n");
-	exit(0);
+        printf("Price must be from 30 to 105 cents inclusive\n");
+        exit(0);
 }
 if (price%5!=0){
-	printf("Price must be a multiple of 5.\n");
-	exit(0);
+        printf("Price must be a multiple of 5.\n");
+        exit(0);
 }
+while (choice!='e' | choice !='E'){
 printf("Welcome to my C Pop Machine!\n");
 printf("\nPop is %d cents. Please insert any combination of ",price);
 printf("nickels [N or n], dimes [D or d] or quarters [Q or q]. ");
 printf("You can also press R or r for coin return.\n");
 
-while(coins<price){
-	printf("\nEnter coin (NDQR): \n");
-	machine(coins,price);
+while(done==0){
+        printf("\nEnter coin (NDQR): ");
+        scanf(" %c", &choice);
+        if (choice == 'e' | choice == 'E'){
+        break;
+        }
+        done=machine(choice,&coins,price);
 }
-printf("Pop is dispensed. Thank you for your business! Please come again.");
+change(coins,price);
+printf("Shutting down. Goodbye.");
+}
 }
